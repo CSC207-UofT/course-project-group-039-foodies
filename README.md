ef7d6289a4c073e623e9065cd9e4e66d823787af
milica <mkraincanic@gmail.com>
2021-10-12 16:52:09 -0400

    Recipe obj first draft

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-## Replace this README.md!
+## Specification
 
 This is a standard README.md file for you to replace. You'll want to include your own content here!
 
 MD (Markdown) files use [Markdown formatting](https://guides.github.com/features/mastering-markdown/): 
 these look very much like .txt files, so you can write them in a very similar way.
+
--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -1,1 +1,26 @@
-public class Recipe {}
+public class Recipe {
+    String foodName;
+    String foodType;
+    int servings;
+    Arraylist ingredients;
+    String instructions;
+    int rating;
+
+    public Recipe(String foodName, String foodType, int servings, Arraylist ingredients, String instructions) {
+        this.foodName = foodName;
+        this.foodType = foodType;
+        this.servings = servings;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.rating = 0
+    }
+
+    public String toString() {
+        String recipeString = this.foodName + "\n" + this.instructions;
+        return recipeString;
+    }
+
+    public void addRating(int newRating) {
+        this.rating = (this.rating + newRating) / 2;
+    }
+}
