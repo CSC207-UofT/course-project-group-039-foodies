023f9317aaa8a26f6b06179905207e1efc96da0f
dohyunkim02 <dohyoenkim@gmail.com>
2021-10-12 00:25:09 -0400

    RecipeDatabase partially implemented

--- src/main/java/RecipeDatabase.java
+++ src/main/java/RecipeDatabase.java
@@ -1,1 +1,20 @@
+package main.java;
 
+import java.util.ArrayList;
+
+/**
+ * This class stores instances of Recipe in ArrayList.
+ */
+
+public class RecipeDatabase {
+    private ArrayList<Recipe> dataList;
+
+    /**
+     * Constructs an ArrayList and add recipes
+     * @param recipe
+     */
+    public RecipeDatabase(Recipe recipe) {
+        this.dataList.add(recipe);
+        }
+    }
+}
