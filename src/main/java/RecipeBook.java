c08f4b2c8dabeae8b4156365d0a86ce478b69480
michellemlin <michellem.lin@mail.utoronto.ca>
2021-10-14 00:54:28 -0400

    finished the RecipeBook.java code

--- src/main/java/RecipeBook.java
+++ src/main/java/RecipeBook.java
@@ -5,21 +5,39 @@ import java.util.HashMap;
  *
  */
 public class RecipeBook {
-    private HashMap<String, Recipe> recipebook;
+    private final HashMap<String, Recipe> recipebook;
 
-
+    /**
+     * Instantiate an empty Recipe Book
+     */
     public RecipeBook() {
         this.recipebook = new HashMap<>();
     }
 
+    /**
+     * Add a Recipe recipe with code recipe code to the Recipe Book
+     *
+     * @param recipecode - a unique String code identifier for Recipe
+     * @param recipe - the recipe
+     */
     public void addRecipe(String recipecode, Recipe recipe) {
         this.recipebook.put(recipecode, recipe);
     }
 
+    /**
+     * Remove the recipe with recipecode from recipebook.
+     *
+     * @param recipecode - a unique String code identifier for Recipe
+     */
     public void removeRecipe(String recipecode) {
         this.recipebook.remove(recipecode);
     }
 
+    /**
+     *
+     * @param recipecode - a unique String code identifier for Recipe
+     * @return - return the recipe with recipecode
+     */
     public Recipe getRecipe(String recipecode) {
         return this.recipebook.get(recipecode);
     }
