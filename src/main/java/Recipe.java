d856d091394ec8ae478c9aa4331a512b0fc9deb4
milica <mkraincanic@gmail.com>
2021-10-14 11:19:10 -0400

    Recipe obj first draft

--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -1,18 +1,27 @@
 public class Recipe {
     String foodName;
+    Integer recipeCode
     String foodType;
     int servings;
-    Arraylist ingredients;
+    ArrayList<String> ingredients;
     String instructions;
-    int ratingTotal;
-    Arraylist ratingList;
 
-    public Recipe(String foodName, String foodType, int servings, Arraylist ingredients, String instructions) {
+
+    public Recipe(int recipeCode, String foodName, String foodType, int servings, ArrayList<String> ingredients, String instructions) {
+        this.recipeCode = recipeCode;
         this.foodName = foodName;
         this.foodType = foodType;
         this.servings = servings;
         this.ingredients = ingredients;
         this.instructions = instructions;
+
+    /**public Recipe(int recipeCode) {
+        this.recipeCode = recipeCode;
+        this.foodName = new String();
+        this.foodType = new String();
+        this.servings = 0;
+        this.ingredients = new ArrayList<String> ();
+        this.instructions = new String(); */
     }
 
     public String toString() {
--- src/main/java/RecipeGateway.java
+++ src/main/java/RecipeGateway.java
@@ -1,1 +1,4 @@
+public class RecipeGateway {
 
+    public RecipeGateway()
+}
