0d71b7a75fb0bc6af23fb505d5bcca20670633a5
milica <mkraincanic@gmail.com>
2021-10-14 21:38:52 -0400

    Add to Recipe and RecipeGateway

--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -1,11 +1,12 @@
 public class Recipe {
+    /** Creates a Recipe object */
+
     String foodName;
     Integer recipeCode
     String foodType;
     int servings;
     ArrayList<String> ingredients;
     String instructions;
-
 
     public Recipe(int recipeCode, String foodName, String foodType, int servings, ArrayList<String> ingredients, String instructions) {
         this.recipeCode = recipeCode;
@@ -14,19 +15,15 @@ public class Recipe {
         this.servings = servings;
         this.ingredients = ingredients;
         this.instructions = instructions;
-
-    /**public Recipe(int recipeCode) {
-        this.recipeCode = recipeCode;
-        this.foodName = new String();
-        this.foodType = new String();
-        this.servings = 0;
-        this.ingredients = new ArrayList<String> ();
-        this.instructions = new String(); */
     }
 
     public String toString() {
+        /** Returns a formatted Recipe for the user to read */
+
         String recipeString = this.foodName + "\n" + this.instructions;
         return recipeString;
     }
+
+    public String getFoodType() { return this.foodType; }
 
 }
--- src/main/java/RecipeGateway.java
+++ src/main/java/RecipeGateway.java
@@ -1,4 +1,31 @@
-public class RecipeGateway {
+package main.java;
 
-    public RecipeGateway()
+import java.util.Arrays;
+
+public class RecipeGateway {
+    /**
+     * Builds a RecipeGateway to convert raw recipe files to Recipe object and store them in the recipe database.
+     * */
+
+    DatabaseManager workingDatabase;
+    Object[][] setOfRecipes;
+
+    public RecipeGateway(DatabaseManager workingDatabase, Object[][] setOfRecipes) {
+        this.workingDatabase = workingDatabase;
+        this.setOfRecipes = setOfRecipes
+    }
+
+    public void BuildRecipe() {
+        /** Take data from setOfRecipes and create Recipe objects to be stored in the recipe database. */
+
+        for(int i = 0; i < setOfRecipes.length; i++) {
+            Integer recipeCode = workingDatabase.getHighestKey();
+            Recipe newRecipe = new Recipe(recipeCode, setOfRecipes[i][0], setOfRecipes[i][1], setOfRecipes[i][2], setOfRecipes[i][3], setOfRecipes[i][4]);
+            workingDatabase.addRecipe(newRecipe);
+        }
+    }
+
+
+
+
 }
