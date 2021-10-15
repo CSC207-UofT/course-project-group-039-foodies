7754a3b0b58640fdaed43af162dd2bde69f01bc4
hjovi1 <64193764+hjovi1@users.noreply.github.com>
2021-10-15 18:36:01 -0400

    Working on implementing PreferenceBook.

--- src/main/java/Entities/PreferenceBook.java
+++ src/main/java/Entities/PreferenceBook.java
@@ -5,12 +5,4 @@ public class PreferenceBook{
     private static String omit;
     private static String include;
     private static String time;
-    private final Object User = new Object();
-
-    //public PreferenceBook() {
-    //this.type =  ((User).restrictions);
-    //this.omit = lastname;
-    //this.include = username;
-    //this.time = email;
-    //}
 }
