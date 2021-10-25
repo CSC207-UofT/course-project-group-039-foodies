4db4a0f615bb1cd2e3383d862c8ff4d2d047803e
Mark Bedaywi <mac02@outlook.com>
2021-10-25 00:10:50 -0400

    Added a javadoc to the createRecipe method

--- src/main/java/UseCases/RecipeFactory.java
+++ src/main/java/UseCases/RecipeFactory.java
@@ -7,6 +7,14 @@ import java.util.ArrayList;
 public class RecipeFactory {
     private static int nextRecipeCode;
 
+    /**
+     * @param name The name of the recipe
+     * @param type The type of the recipe
+     * @param servings The number of people the recipe serves
+     * @param ingredients The ingredients in the recipe
+     * @param instructions The instructions for creating the recipe
+     * @return The created recipe
+     */
     public static Recipe createRecipe(String name, String type, int servings,
                                ArrayList<String> ingredients, String instructions) {
         Recipe newRecipe = new Recipe(nextRecipeCode, name, type, servings, ingredients, instructions);
