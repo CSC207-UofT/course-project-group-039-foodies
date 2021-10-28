8e7669066d023354f5e42c0461a8cbb85e0592cc
dohyunkim02 <dohyoenkim@gmail.com>
2021-10-28 12:31:44 -0400

    Added Javadocs and implemented Sort interface, ServingsSort class, ServingsComparator class, and ServingsSortTest.

--- src/main/java/Filters/FoodTypeFilter.java
+++ src/main/java/Filters/FoodTypeFilter.java
@@ -3,15 +3,25 @@ package main.java.Filters;
 import main.java.Entities.Recipe;
 import java.util.ArrayList;
 
+/**
+ * Filter Recipes based on the food type.
+ */
 public class FoodTypeFilter implements Filter{
     private final Recipe[] data;
     private final String foodType;
 
+    /**
+     * @param data Array of Recipes.
+     * @param foodType Food type (e.g. "Breakfast" or "Dinner").
+     */
     public FoodTypeFilter(Recipe[] data, String foodType) {
         this.data = data;
         this.foodType = foodType;
     }
 
+    /**
+     * @return Array of Recipes that is completely filtered based on the food type.
+     */
     @Override
     public Recipe[] filter() {
         ArrayList<Recipe> result = new ArrayList<>();
