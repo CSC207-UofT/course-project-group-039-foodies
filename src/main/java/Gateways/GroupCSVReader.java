eef5ebb6abca32b0b177d0f8fab831e83586fd45
judyhayun1102 <90357869+judyhayun1102@users.noreply.github.com>
2021-11-09 04:53:57 -0500

    Create GroupCSVReader.java

---
+++ src/main/java/Gateways/GroupCSVReader.java
@@ -0,0 +1,10 @@
+package main.java.Gateways;
+
+import main.java.Entities.Group;
+import main.java.UseCases.GroupFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GroupCSVReader extends CSVReader {
+}
