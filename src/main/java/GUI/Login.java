1dcd1b091431bb3107ad32a9c457d9881567c113
hjovi1 <64193764+hjovi1@users.noreply.github.com>
2021-11-10 20:56:09 -0500

    Aligned all JButtons and JLabels. Now able to walk-through up until Menu.

--- src/main/java/GUI/Login.java
+++ src/main/java/GUI/Login.java
@@ -27,7 +27,6 @@ public class Login {
 	private void initialize() {
 
 		frame = new JFrame();
-
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Recipick");
