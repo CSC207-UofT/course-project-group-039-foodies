ca2cf82d1c5b5ac21587a0af33db99a83b8c05a7
Dohyun Kim <34140538+dohyunkim02@users.noreply.github.com>
2021-11-13 23:35:00 -0500

    Update ServingsFilterTest.java

--- src/test/java/FilterTests/ServingsFilterTest.java
+++ src/test/java/FilterTests/ServingsFilterTest.java
@@ -30,8 +30,8 @@ public class ServingsFilterTest {
         recipes.addRecipe(recipe2);
         recipes.addRecipe(recipe3);
 
-        ServingsFilter servings = new ServingsFilter(recipes.getRecipes(), 2);
-        Recipe[] filtered = servings.filter();
+        ServingsFilter servings = new ServingsFilter(2);
+        Recipe[] filtered = servings.filter(recipes.getRecipes());
 
         assertEquals(1, filtered.length);
         assertEquals("food2", filtered[0].getName());
