5abc8b9165773bfa3e8aedacc8cb0a088e1e1f1e
Dohyun Kim <34140538+dohyunkim02@users.noreply.github.com>
2021-11-13 23:32:46 -0500

    Update RatingSort.java

--- src/main/java/Sorts/RatingSort.java
+++ src/main/java/Sorts/RatingSort.java
@@ -7,21 +7,13 @@ import java.util.Arrays;
  * Sort Recipes based on the ratings of the recipe (from high to low).
  */
 public class RatingSort implements Sort {
-    private final Recipe[] data;
-
-    /**
-     * @param data Array of Recipes.
-     */
-    public RatingSort(Recipe[] data) {
-        this.data = data;
-    }
 
     /**
      * @return Array of Recipes that is completely sorted based on the ratings.
      */
     @Override
-    public Recipe[] sort() {
-        Recipe[] result = this.data.clone();
+    public Recipe[] sort(Recipe[] recipes) {
+        Recipe[] result = recipes.clone();
         Arrays.sort(result, new RatingComparator());
         return result;
     }
