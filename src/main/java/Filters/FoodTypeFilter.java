88ecf3e97fba099335142ed3382932b946b68f5c
Dohyun Kim <34140538+dohyunkim02@users.noreply.github.com>
2021-11-13 23:28:26 -0500

    Update FoodTypeFilter.java

--- src/main/java/Filters/FoodTypeFilter.java
+++ src/main/java/Filters/FoodTypeFilter.java
@@ -7,15 +7,12 @@ import java.util.ArrayList;
  * Filter Recipes based on the food type.
  */
 public class FoodTypeFilter implements Filter{
-    private final Recipe[] data;
     private final String foodType;
 
     /**
-     * @param data Array of Recipes.
      * @param foodType Food type (e.g. "Breakfast" or "Dinner").
      */
-    public FoodTypeFilter(Recipe[] data, String foodType) {
-        this.data = data;
+    public FoodTypeFilter(String foodType) {
         this.foodType = foodType;
     }
 
@@ -23,9 +20,9 @@ public class FoodTypeFilter implements Filter{
      * @return Array of Recipes that is completely filtered based on the food type.
      */
     @Override
-    public Recipe[] filter() {
+    public Recipe[] filter(Recipe[] recipes) {
         ArrayList<Recipe> result = new ArrayList<>();
-        for (Recipe recipe : data) {
+        for (Recipe recipe : recipes) {
             if (recipe.getFoodType().equals(foodType)) {
                 result.add(recipe);
             }
