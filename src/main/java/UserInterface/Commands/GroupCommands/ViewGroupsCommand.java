b0ca965bcb16d2128ca7d53474909ae57054f6f4
judyhayun1102 <hayun.kim@mail.utoronto.ca>
2021-12-05 09:37:32 -0500

    added and modified some GroupRecipeBook classes

---
+++ src/main/java/UserInterface/Commands/GroupCommands/ViewGroupsCommand.java
@@ -0,0 +1,18 @@
+package main.java.UserInterface.Commands.GroupCommands;
+
+import main.java.Gateways.GroupCSVReader;
+import main.java.UserInterface.Commands.Command;
+import main.java.UserInterface.UserInterface;
+
+
+public class ViewGroupsCommand extends Command {
+    public ViewGroupsCommand()  {super("view my groups",
+            "Displays all the groups that you've joined");}
+
+    @Override
+    public void runAction(UserInterface UI) {
+        String username = UI.getUser().getUsername();
+        String joinedGroups = GroupCSVReader.getInstance().getJoinedGroups(username);
+        UI.displayMessage(joinedGroups);
+    }
+}
