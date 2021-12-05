211bb3700a1215a4575cad0e8824ec8e82d81943
judyhayun1102 <90357869+judyhayun1102@users.noreply.github.com>
2021-12-05 12:30:17 -0500

    Create GroupRecipeBookCSVReader.java

---
+++ src/main/java/Gateways/GroupRecipeBookCSVReader.java
@@ -0,0 +1,13 @@
+package main.java.Gateways;
+
+import main.java.Entities.*;
+import main.java.UseCases.GroupRecipeBookManager;
+import main.java.UseCases.Utilities.RecipeCollectionFacade;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+
+public class GroupRecipeBookCSVReader extends CSVReader {
+}
