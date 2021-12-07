f0ba724c9a388589c2ce15ad2751592a432e9f90
judyhayun1102 <90357869+judyhayun1102@users.noreply.github.com>
2021-12-07 03:17:30 -0500

    Update AddSubRecipeBookCommand.java

--- src/main/java/UserInterface/Commands/GeneralRecipeBookCommands/PersonalRecipeBookCommands/AddSubRecipeBookCommand.java
+++ src/main/java/UserInterface/Commands/GeneralRecipeBookCommands/PersonalRecipeBookCommands/AddSubRecipeBookCommand.java
@@ -1,4 +1,4 @@
-package main.java.UserInterface.Commands.GeneralRecipeBookCommands.PersonalRecipeBookCommands;
+package main.java.UserInterface.Commands.RecipeBookCommands;
 
 import main.java.Entities.User;
 import main.java.Gateways.RecipeBookCSVReader;
@@ -9,7 +9,7 @@ import main.java.UserInterface.UserInterface;
 public class AddSubRecipeBookCommand extends Command {
 
     public AddSubRecipeBookCommand() {
-        super("add a subrecipebook", "Adds a new sub recipe book to the user's recipe book");
+        super("add a sub recipe book", "Adds a new sub recipe book to the user's recipe book");
     }
 
     @Override
@@ -23,11 +23,11 @@ public class AddSubRecipeBookCommand extends Command {
         User user = UI.getUser();
         String username = user.getUsername();
         if (!RecipeBookCSVReader.getInstance().isSubRecipeBook(username, subRecipeBookName)) {
-            RecipeBookCSVReader.getInstance().addnewSubRecipeBook(user, subRecipeBookName, subRecipeBookDesc);
+            RecipeBookCSVReader.getInstance().addNewSubRecipeBook(user, subRecipeBookName, subRecipeBookDesc);
             UI.displayMessage("New SubRecipeBook with name " + subRecipeBookName + " and description " + subRecipeBookDesc
                     + " created successfully");
         } else {
-            UI.displayMessage("A subrecipebook with the name " + subRecipeBookName + " already exists");
+            UI.displayMessage("A SubRecipeBook with the name " + subRecipeBookName + " already exists");
         }
     }
 }
