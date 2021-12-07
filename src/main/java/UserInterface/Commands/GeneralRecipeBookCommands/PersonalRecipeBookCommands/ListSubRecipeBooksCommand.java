3d9bf5d1ce749c1ac6b181151d8d327f7bee20d4
judyhayun1102 <90357869+judyhayun1102@users.noreply.github.com>
2021-12-07 03:21:34 -0500

    Update ListSubRecipeBooksCommand.java

--- src/main/java/UserInterface/Commands/GeneralRecipeBookCommands/PersonalRecipeBookCommands/ListSubRecipeBooksCommand.java
+++ src/main/java/UserInterface/Commands/GeneralRecipeBookCommands/PersonalRecipeBookCommands/ListSubRecipeBooksCommand.java
@@ -1,13 +1,15 @@
-package main.java.UserInterface.Commands.GeneralRecipeBookCommands.PersonalRecipeBookCommands;
+package main.java.UserInterface.Commands.RecipeBookCommands;
 
+import main.java.Entities.RecipeBook;
 import main.java.Entities.SubRecipeBook;
+import main.java.Gateways.RecipeBookCSVReader;
 import main.java.UseCases.RecipeBookManager;
 import main.java.UserInterface.Commands.Command;
 import main.java.UserInterface.UserInterface;
 
 public class ListSubRecipeBooksCommand extends Command {
     public ListSubRecipeBooksCommand() {
-        super("show subrecipebooks", "show all the SubRecipeBooks contained in RecipeBook");
+        super("show sub recipe books", "show all the SubRecipeBooks contained in RecipeBook");
     }
 
     @Override
@@ -18,4 +20,6 @@ public class ListSubRecipeBooksCommand extends Command {
         }
     }
 }
+
+
 
