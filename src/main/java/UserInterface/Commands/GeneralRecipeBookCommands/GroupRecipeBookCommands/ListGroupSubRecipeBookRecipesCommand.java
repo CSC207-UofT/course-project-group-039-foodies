415df583ea874ae0bf91577a6972d540c139f55a
yiemily <90373509+yiemily@users.noreply.github.com>
2021-12-07 05:32:35 -0500

    Update ListGroupSubRecipeBookRecipesCommand.java

--- src/main/java/UserInterface/Commands/GeneralRecipeBookCommands/GroupRecipeBookCommands/ListGroupSubRecipeBookRecipesCommand.java
+++ src/main/java/UserInterface/Commands/GeneralRecipeBookCommands/GroupRecipeBookCommands/ListGroupSubRecipeBookRecipesCommand.java
@@ -1,7 +1,10 @@
 package main.java.UserInterface.Commands.GeneralRecipeBookCommands.GroupRecipeBookCommands;
+import main.java.Entities.Group;
 import main.java.Entities.Recipe;
-import main.java.UseCases.RecipeBookManager;
-import main.java.UseCases.SubRecipeBookManager;
+import main.java.Entities.User;
+import main.java.Gateways.GroupCSVReader;
+import main.java.UseCases.GroupRecipeBookManager;
+import main.java.UseCases.GroupSubRecipeBookManager;
 import main.java.UserInterface.Commands.Command;
 import main.java.UserInterface.UserInterface;
 
@@ -12,11 +15,16 @@ public class ListGroupSubRecipeBookRecipesCommand extends Command{
 
     @Override
     public void runAction(UserInterface UI) {
+        String groupName = UI.queryUser("Enter the name of the group");
+        User user = UI.getUser();
+        String username = user.getUsername();
+        Group group = GroupCSVReader.getInstance().getGroup(groupName, username);
+
         String subRecipeBookName = UI.queryUser("Please confirm the recipe book you would like to see recipes for");
-        RecipeBookManager recipebookmanager = new RecipeBookManager(UI.getUser());
-        if (recipebookmanager.containsSubRecipeBook(subRecipeBookName)) {
-            SubRecipeBookManager subRecipeBookManager = new SubRecipeBookManager(
-                    recipebookmanager.findsubrecipebook(subRecipeBookName));
+        GroupRecipeBookManager groupRecipeBookManager = new GroupRecipeBookManager(group);
+        if (groupRecipeBookManager.containsSubRecipeBook(subRecipeBookName)) {
+            GroupSubRecipeBookManager subRecipeBookManager =
+                    new GroupSubRecipeBookManager(groupRecipeBookManager.findSubRecipeBook(subRecipeBookName));
             for (Recipe recipe : subRecipeBookManager.getRecipes()) {
                 UI.displayMessage(recipe.toString());
             }
