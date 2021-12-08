efda5918381f37f9fbcb2aa658e06284d3347948
hjovi1 <64193764+hjovi1@users.noreply.github.com>
2021-12-08 18:34:36 -0500

    GetNewTopPicksCommand updated.

---
+++ src/main/java/UserInterface/Commands/RecipeViewerCommands/GetNewTopPickCommand.java
@@ -0,0 +1,45 @@
+package main.java.UserInterface.Commands.RecipeViewerCommands;
+
+import main.java.Entities.RecipeCollection;
+import main.java.UseCases.Utilities.RecipeCollectionFacade;
+import main.java.UserInterface.Commands.Command;
+import main.java.Entities.Recipe;
+import main.java.UserInterface.UserInterface;
+
+import java.util.Iterator;
+
+/**
+ * Allows the user to view a new recipe to rate
+ */
+public class GetNewTopPickCommand extends Command {
+    private static Iterator<Recipe> recipeIterator; // Uses the lazy loading design pattern to be instantiated
+    private static boolean isIteratorSet = false;
+
+    public GetNewTopPickCommand() {
+        super("get new recipe", "Gets a new recipe to rate");
+    }
+
+    @Override
+    public void runAction(UserInterface UI) {
+        // If the iterator hasn't been set yet, or we have already iterated over all elements
+        if (!isIteratorSet || !recipeIterator.hasNext()) {
+            initializeIterator(UI);
+        }
+
+        if (recipeIterator.hasNext()) {
+            UI.displayMessage(recipeIterator.next().toString());
+        }
+    }
+
+    /**
+     * Sets the iterator of Recipes to go through,
+     * by taking the current RecipeCollection stored in the UI
+     * @param UI The userInterface being used
+     */
+    public static void initializeIterator(UserInterface UI) {
+        isIteratorSet = true;
+        RecipeCollection recipes = UI.getRecipeCollection();
+        recipes.removeNonTopPicks(UI.getUser().getRecipeBook());
+        recipeIterator = RecipeCollectionFacade.getIterator(recipes);
+    }
+}
