6eed95165575688de57ff7658b504c8f16363ecf
milica <mkraincanic@gmail.com>
2021-12-08 16:09:22 -0500

    Testing

--- src/test/java/PreferenceBookTests.java
+++ src/test/java/PreferenceBookTests.java
@@ -7,6 +7,14 @@ import static org.junit.Assert.*;
 public class PreferenceBookTests {
     PreferenceBook preferences = new PreferenceBook("TestUser");
 
+    @Test
+    public void addDietTest() {
+        preferences.addDiet("Vegan");
+        assertEquals(preferences.getDiet(), "Vegan");
+    }
 
-
+    @Test
+    public void containsTest() {
+        assertFalse(preferences.contains("omit", "eggs"));
+    }
 }
